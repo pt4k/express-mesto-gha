c137e6eea0826d1fb9b2e9e0c6255800e8d183df
Yuriy Pishchenko <locbne@gmail.com>
2022-08-16 17:26:21 +0300

    Добавил ошибки 400 и 404

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,6 @@ const createUser = (req, res) => {
   User.create(req.body)
     .then((user) => res.status(201).send(user))
     .catch((err) => {
-      console.log(err.name);
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: 'Переданы некорректные данные в методы создания карточки, пользователя, обновления аватара пользователя или профиля.' });
       } else {
@@ -16,10 +15,20 @@ const createUser = (req, res) => {
 const getUser = (req, res) => {
   User.findById(req.params.id)
     .orFail(() => {
-      throw new Error('Карточка или пользователь не найден');
+      const error = new Error('Пользователь по заданному id отсутствует в базе');
+      error.statusCode = 404;
+      throw error;
     })
     .then((user) => res.status(200).send(user))
-    .catch((err) => res.status(500).send({ message: `Ошибка сервера: ${err}` }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(400).send({ message: 'Указан некорректный id пользователя' });
+      } else if (err.statusCode === 404) {
+        res.status(404).send({ message: 'Пользователь по заданному id отсутствует в базе' });
+      } else {
+        res.status(500).send({ message: `Ошибка сервера: ${err}` });
+      }
+    });
 };
 
 const getUsers = (req, res) => {
@@ -33,14 +42,23 @@ const patchUser = (req, res) => {
   const id = req.user._id;
   User.findByIdAndUpdate(id, { name, about }, { new: true, runValidators: true, upsert: true })
     .then((user) => res.status(200).send({ data: user }))
-    .catch((err) => res.status(500).send({ message: `Запрашиваемый пользователь не найден. Ошибка: ${err}` }));
+    .catch((err) => {
+      console.log(err.name);
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные в методы создания карточки, пользователя, обновления аватара пользователя или профиля.' });
+      } else {
+        res.status(500).send({ message: `Ошибка сервера: ${err}` });
+      }
+    });
 };
 
 const patchAvatar = (req, res) => {
   const { avatar } = req.body;
-  const id = req.user._id;
-  User.findByIdAndUpdate(id, avatar, { new: true, runValidators: true })
-    .then((user) => res.send({ data: user }))
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+    .then((user) => {
+      res.send({ data: user });
+      console.log(user);
+    })
     .catch((err) => res.status(500).send({ message: `Запрашиваемый пользователь не найден. Ошибка: ${err}` }));
 };
 

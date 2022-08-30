2fb1b0615609b8fe30e8de0c2c5e469095cd15ad
Yuriy Pishchenko <locbne@gmail.com>
2022-08-30 14:20:56 +0300

    Валидация запросов на сервер

--- app.js
+++ app.js
@@ -26,6 +26,9 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required(),
     password: Joi.string().required().min(8),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string(),
   }),
 }), createUser);
 

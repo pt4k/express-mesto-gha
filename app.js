7b270a069c8751f2ba05afb5949f559eedf5d157
Yuriy Pishchenko <locbne@gmail.com>
2022-09-18 17:33:38 +0300

    fix cors

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 const options = {
   origin: [
-    'http://localhost:3005',
+    'http://pishchenko.mesto.students.nomoredomains.sbs',
+    'https://pishchenko.mesto.students.nomoredomains.sbs',
+    'http://api.pt4k.mesto.students.nomoredomains.sbs',
     'https://api.pt4k.mesto.students.nomoredomains.sbs',
   ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
